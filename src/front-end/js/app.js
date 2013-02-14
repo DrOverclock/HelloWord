75d57b9ffc554f470e749e79f388b8236f68e215
Danil Tumaykin <danil.tumaykin@provenia.it>
2013-02-15 00:03:56 +0100

    Added handle bars. Sample data for top and top 5 templating.

--- src/front-end/js/app.js
+++ src/front-end/js/app.js
@@ -1,1 +1,17 @@
 // actual application here
+
+(function() {
+	// handle bars template compilation
+	var source   = $("#top-table-entry").html();
+	var hbTemplates = {}; // array of templates
+	hbTemplates["top-table-entry"] = Handlebars.compile(source);
+
+	// get and bind top 5 table
+	getTop5(function (result) {
+		var t = $("#top5"); // get table element
+		for (var i = 0 ; i < result.length; i++) {
+			// compile, create and append
+			t.append(hbTemplates["top-table-entry"](result[i]));
+		};
+	});
+})();
--- src/front-end/js/fobia-api.js
+++ src/front-end/js/fobia-api.js
@@ -1,1 +1,33 @@
 // api for itisfobia web service
+
+// Gets top 5 players, then calls `callback` with responce array as argument. Players are ordered by score in descending order.
+function getTop5 (callback) {
+	// TODO: replace with actual data
+	callback([
+			{
+				number: 1,
+				name: "Player 0",
+				score: "10"
+			},
+			{
+				number: 2,
+				name: "Player 1",
+				score: "9"
+			},
+			{
+				number: 3,
+				name: "Player 2",
+				score: "8"
+			},
+			{
+				number: 4,
+				name: "Player 3",
+				score: "7"
+			},
+			{
+				number: 5,
+				name: "Player 4",
+				score: "6"
+			}
+		]);
+};

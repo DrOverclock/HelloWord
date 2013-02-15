447cf518ad3297a301f90e5f9557e226d865421f
Danil Tumaykin <danil.tumaykin@provenia.it>
2013-02-15 19:55:19 +0100

    Added user registration, and error handling.

--- src/front-end/js/app.js
+++ src/front-end/js/app.js
@@ -8,11 +8,16 @@ application = {};
 	a.init = function () {
 		if(a.initialized) return;
 
+		// data about user
+		a.session = {};
+		a.session.nav = []; // navigation stack
+
 		// handle bars template compilation
 		a.hbTemplates = {}; // array of templates
 		a.hbTemplates["top-table-entry"] = Handlebars.compile($("#top-table-entry").html());
 		a.hbTemplates["auth-frame"] = Handlebars.compile($("#auth-frame").html());
 		a.hbTemplates["home-frame"] = Handlebars.compile($("#home-frame").html());
+		a.hbTemplates["error-frame"] = Handlebars.compile($("#error-frame").html());
 
 		a.initialized = true;
 	}
@@ -20,7 +25,7 @@ application = {};
 	// startes the application
 	a.start = function () {
 		// get and bind top 5 table
-		getTop5(function (result) {
+		api.chartRequest(function (result) {
 			var t = $("#top5"); // get table element
 			for (var i = 0 ; i < result.length; i++) {
 				// compile, create and append
@@ -34,9 +39,26 @@ application = {};
 
 	// pushes UC to screen, binds data and binds related events
 	a.pushUC = function(frame, data) {
-		var uc = $("#uc").html("");
-		uc.append(a.hbTemplates[frame](data));
-		a.bindUC(frame);
+		var uc = $("#uc");
+		uc.fadeToggle()
+
+		setTimeout(function() {
+			uc.html("");
+			uc.append(a.hbTemplates[frame](data));
+			a.bindUC(frame);
+		}, 400);
+
+		uc.fadeToggle();
+		a.session.nav.push({
+			"frame": frame,
+			"data": data
+		});
+	};
+
+	a.goBack = function() {
+		a.session.nav.pop(); // remove current
+		var last = a.session.nav.pop(); // remove precedent
+		a.pushUC(last.frame, last.data); // repush precedent
 	};
 
 	// bind UCs controls callbacks
@@ -45,20 +67,76 @@ application = {};
 			case "auth-frame":
 				$("form").on("submit", function(e) {
 					e.preventDefault();
-					authenticate(
-						$("#authUsername").val(),
-						$("#authPassword").val(),
-						a.handleAuthentication);
+
+					console.log("submit!");
+
+					// normal auth request
+					if(a.session["auth-state"] != "verify") {
+						a.session["login"] = $("#authUsername").val();
+						a.session["password"] = $("#authPassword").val();
+
+						api.authRequest(
+							a.session["login"],
+							a.session["password"],
+							a.handleAuthentication);
+					}
+
+					// registration verification request
+					else {
+						a.session["login"] = $("#authUsername").val();
+						a.session["password"] = $("#authPassword").val();
+						a.session["name"] = $("#authName").val();
+						a.session["surname"] = $("#authSurname").val();
+
+						api.verifyRequest(
+							a.session["login"],
+							a.session["password"],
+							a.session["name"],
+							a.session["surname"],
+							a.handleAuthentication);
+					}
+
 				});
  				break;
+			case "error-frame":
+				$("#goBackButton").on("click", function() {a.goBack()});
+				break; 				
 		};
 	};
 
 	/// UX HANDLERS
 	a.handleAuthentication = function(result) {
+
 		console.log(result);
-		if(result.status == "ok")
-			a.pushUC("home-frame", result.data);
+		if(result.status == "ok") {
+			// make a request for user and the push screen with result
+			api.userdataRequest(function(r) {
+				if(r.status == "ok") {
+					a.session["auth-state"] = "ok";
+					a.pushUC("home-frame", r);
+				}
+				else
+					a.pushUC("error-frame", {error: "Errore durante l'autentificazione."});
+			});
+		}
+
+		else if(result.status == "verify") { // user exists in db, need additional data
+			a.session["auth-state"] = "verify";
+
+			// show the registration form
+			$("form").find(":hidden").each(function() {$(this).slideToggle();});
+			$("#authButton").text("Iscriviti");
+		}
+
+		else if(result.status == "fail")
+		{
+			a.pushUC("error-frame", {error: "Errore durante l'autentificazione."});
+		}
+
+		else // unavailable
+		{
+			a.pushUC("error-frame", {error: "Server d'autentificazione attualmente non disponibile, riprovare piu' tardi."});
+		}
 	};
 
 })(application);
--- src/front-end/js/fobia-api.js
+++ src/front-end/js/fobia-api.js
@@ -1,42 +1,4 @@
 // api for itisfobia web service
-
-/***
-
-Gets top 5 players, then calls `callback` with responce array as argument. 
-Players are ordered by score in descending order.
-
-***/
-function getTop5 (callback) {
-	// TODO: replace with actual data
-	callback([
-			{
-				number: 1,
-				name: "Player 0",
-				score: "10"
-			},
-			{
-				number: 2,
-				name: "Player 1",
-				score: "9"
-			},
-			{
-				number: 3,
-				name: "Player 2",
-				score: "8"
-			},
-			{
-				number: 4,
-				name: "Player 3",
-				score: "7"
-			},
-			{
-				number: 5,
-				name: "Player 4",
-				score: "6"
-			}
-		]);
-};
-
 /***
 
 Authenticates a user with `username` and `password`, then calls the `callback`.
@@ -48,16 +10,128 @@ function authenticate(username, password, callback) {
 	if(username == 'dan' && password == "tuma")
 	{
 		callback({
-			status: "ok",
-			data: {
-				target: "Francesco Zanini"
-			}
+			status: "ok"
 		});
 	}
 	else
 	{
 		callback({
-			status: "fail"
+			status: "verify"
 		});
 	}
 }
+
+api = {};
+
+(function(f){
+
+	/***
+
+	Gets top 5 players, then calls `callback` with responce array as argument. 
+	Players are ordered by score in descending order.
+
+	***/
+	f.chartRequest = function(callback) {
+		// TODO: replace with actual data
+		callback([
+				{
+					number: 1,
+					name: "Player 0",
+					score: "10"
+				},
+				{
+					number: 2,
+					name: "Player 1",
+					score: "9"
+				},
+				{
+					number: 3,
+					name: "Player 2",
+					score: "8"
+				},
+				{
+					number: 4,
+					name: "Player 3",
+					score: "7"
+				},
+				{
+					number: 5,
+					name: "Player 4",
+					score: "6"
+				}
+			]);
+	};
+
+
+	/***
+
+	Authenticates a user with `username` and `password`, then calls the `callback`.
+	Object with state and user data is passed to `callback`.
+
+	***/
+	f.authRequest = function(username, password, callback) {
+		// TODO: replace with actual authentication
+		console.log("auth!");
+		if(username == 'dan')
+		{
+			callback({
+				status: "ok"
+			});
+		}		
+		else if(username == 'cesco')
+		{
+			callback({
+				status: "verify"
+			});
+		}		
+		else if(username == 'patrick')
+		{
+			callback({
+				status: "fail"
+			});
+		}
+		else
+		{
+			callback({
+				status: "unavailable"
+			});
+		}
+	};
+
+	/***
+
+	Submits a user data to server, then calls `callback` with result.
+
+	***/
+	f.verifyRequest = function(username, password, name, surname, callback) {
+		console.log("verify!");
+		// TODO: actual code
+		if(username == "dan")
+		{
+			callback({
+				status: "ok"
+			});
+		}
+		else
+		{
+			callback({
+				status: "fail"
+			});
+		}
+	};
+
+
+	/***
+
+	Gets user data for current user, then calls `callback` with result.
+
+	***/
+	f.userdataRequest =  function (callback) {
+		console.log("get user data!");
+		callback({
+			status: "ok",
+			target: "verona"
+		});
+	};
+
+})(api);
